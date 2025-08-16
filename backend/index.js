76a63166ee2e357452a1e6a5fcefd0bc0a3b582f
RaghavBansal1211 <raghavbansal1211@gmail.com>
2025-08-16 17:16:08 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,13 @@ setupSocket(server);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+
+app.use(
+  cors({
+    origin: ["https://uudra.in", "http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 
 
--- backend/utils/sockets.js
+++ backend/utils/sockets.js
@@ -5,7 +5,7 @@ let io;
 const setupSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: 'http://localhost:3000',
+            origin: ["https://uudra.in", "http://localhost:3000"],
             credentials: true
         }
     });
