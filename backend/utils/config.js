efaa28108b83d3a3d4f674b5c33dd366a189dbff
Khushi Mehta <khushimehta82339@gmail.com>
2025-08-21 07:59:01 +0530

    bug fixed

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,7 +2,6 @@ const mongoose  = require("mongoose");
 
 async function connectDB(url){
     await mongoose.connect(url).then( () =>{console.log("Database connected successfully")}).catch((err) => {console.error(err)});
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 }
 
