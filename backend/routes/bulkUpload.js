0bdcebe07afed30d8939868309997af6e7264ca6
RaghavBansal1211 <raghavbansal1211@gmail.com>
2025-08-25 18:07:16 +0530

    bulk upload doctor and staff

--- backend/routes/bulkUpload.js
+++ backend/routes/bulkUpload.js
@@ -6,7 +6,9 @@ const router = express.Router();
 const upload = multer({ dest: '/tmp/uploads' }); 
 
 router.post('/speciality', upload.single('file'),bulkUploadSpeciality);
-
 router.post('/department', upload.single('file'), bulkUploadDepartment);
+router.post("/doctors", upload.single("file"), bulkUploadDoctors);
+router.post("/staff", upload.single("file"), bulkUploadStaff);
+
 
 module.exports = router;
