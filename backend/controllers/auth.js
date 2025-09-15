ecb84931f1745877826cd12013b12de7d6be8847
Khushi Mehta <khushimehta82339@gmail.com>
2025-09-15 23:31:05 +0530

    first commit

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 const Doctor = require('../models/Doctor');
 const bcrypt = require('bcrypt');
 const Staff = require('../models/Staff');
